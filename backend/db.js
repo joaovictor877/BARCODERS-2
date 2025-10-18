d673c80896e000d257de7e6ba1f7addb02597692
Joao <silvasouzajoaovictor877@gmail.com>
2025-10-18 12:47:38 -0300

    modulo do pacote node

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise';
 
 // Cria um pool de conexões com o banco de dados
 const pool = mysql.createPool({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'Juca12@!',
   database: 'estoque',
-  port: 3306,
+  port: 3307,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
