5cfa89f5bd40fb6d98f0e39961940193c5de72ff
Joao Victor <silvasouzajoaovictor877@gmail.com>
2025-10-21 15:32:09 -0300

    troca de porta para a do server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 const projectRoot = path.join(__dirname, '..');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const upload = multer();
 
 // --- CONFIGURAÇÃO E MIDDLEWARES PRINCIPAIS ---
