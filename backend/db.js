c1c47531ad03f7982bd8a83e4682246257e1c1ee
Joao <silvasouzajoaovictor877@gmail.com>
2025-12-02 18:31:27 -0300

    banco de dados azure

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise';
 
 // Cria um pool de conexões com o banco de dados
 const pool = mysql.createPool({
-  host: 'shortline.proxy.rlwy.net',
-  user: 'root',
-  password: 'uMnCTGymhFgyqRCnZWoHIjYfDTWOKLnR',
+  host: 'barcoders.mysql.database.azure.com',
+  user: 'adminbar',
+  password: 'Juca12@!#',
   database: 'estoque',
-  port: 51931,
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
