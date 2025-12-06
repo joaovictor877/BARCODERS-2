e300872a1148caebfa30b4d30f627da0d4b443ef
jaozin30718 <jaov30718@gmail.com>
2025-12-05 22:05:45 -0300

    configuração de pool de conexões de banco de dados para MySQL no Azure

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,14 @@ import mysql from 'mysql2/promise';
 
 // Cria um pool de conexões com o banco de dados
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
+  host: 'barcoders.mysql.database.azure.com',
+  user: 'adminbar',
+  password: 'Juca12@!#',
   database: 'estoque',
   port: 3306,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
